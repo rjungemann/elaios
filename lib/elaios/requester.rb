47864742671fe3191d397c206e55d2b863877129
Roger Jungemann <roger@thefifthcircuit.com>
2016-11-04 09:36:28 -0700

    Initial promises functionality

--- lib/elaios/requester.rb
+++ lib/elaios/requester.rb
@@ -7,7 +7,10 @@ class Elaios::Requester
       l.progname = @name
       l.level = ENV['LOG_LEVEL'] || 'info'
     }
+    @callbacks_enabled = options[:callbacks_enabled] || true
+    @promises_enabled = options[:promises_enabled] || false
     @blocks = {}
+    @deferreds = {}
     @in = Simple::Queue.new
     @out = Simple::Queue.new
   end
@@ -39,9 +42,8 @@ class Elaios::Requester
     payload = JSON.dump(data)
     @logger.debug(%(method_missing payload: #{payload.inspect}))
     @out << payload
-    return unless block
-    @logger.debug(%(registered block for id #{id}: #{block.inspect}))
-    @blocks[id] = block if block
+    generate_block(block, data)
+    generate_promise(data) # Return a promise.
   end
 
   # Called internally by `push`. Exposed for testing.
@@ -50,16 +52,51 @@ class Elaios::Requester
     return unless results
     results.split("\n").each do |result|
       @logger.debug(%(result given: #{result}))
-      payload = JSON.load(result) rescue nil
-      next unless payload
-      @logger.debug(%(payload parsed: #{payload.inspect}))
-      block = @blocks[payload['id']]
-      @logger.debug(%(block found: #{block.inspect}))
-      next unless block
-      self.instance_exec(payload, &block)
-      @logger.debug(%(block called: #{block.inspect}))
-      @blocks.delete(payload['id'])
-      @logger.debug(%(block unregistered: #{block.inspect}))
+      data = JSON.load(result) rescue nil
+      next unless data
+      @logger.debug(%(payload parsed: #{data.inspect}))
+      call_block(data)
+      call_promise(data)
     end
+  end
+
+  def generate_block(block, data)
+    return unless @callbacks_enabled
+    id = data['id']
+    return unless block
+    @logger.debug(%(registered block for id #{id}: #{block.inspect}))
+    return unless block
+    @blocks[id] = block
+  end
+
+  def call_block(data)
+    return unless @callbacks_enabled
+    block = @blocks[data['id']]
+    @logger.debug(%(block found: #{block.inspect}))
+    return unless block
+    self.instance_exec(data, &block)
+    @logger.debug(%(block called: #{block.inspect}))
+    @blocks.delete(data['id'])
+    @logger.debug(%(block unregistered: #{block.inspect}))
+  end
+
+  def generate_promise(data)
+    return unless @promises_enabled
+    id = data['id']
+    @logger.debug(%(generating a deferred and promise for id #{id}))
+    @deferreds[id] = EM::Q.defer
+    @deferreds[id].promise # Return the promise.
+  end
+
+  def call_promise(data)
+    return unless @promises_enabled
+    id = data['id']
+    deferred = @deferreds[id]
+    return unless deferred
+    @logger.debug(%(deferred found: #{deferred.inspect}))
+    deferred.resolve(data)
+    @logger.debug(%(deferred resolved: #{deferred.inspect}))
+    @deferred.delete(id)
+    @logger.debug(%(deferred unregistered: #{deferred.inspect}))
   end
 end
